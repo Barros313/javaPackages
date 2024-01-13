fb1441b3f8c3ee717b07ba1683e011961295255d
Barros313 <gbarros1337@gmail.com>
2024-01-12 23:17:35 -0300

    Week 1 - Mario Less

---
+++ exercises/MarioLess.java
@@ -0,0 +1,37 @@
+package exercises;
+
+import utilities.*;
+
+import java.util.Scanner;
+
+public class MarioLess {
+    public static void marioLess(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        Clear.clearScreen();
+        
+        Integer height = 0;
+
+        while (true) {
+            height = Input.getInteger("Height: ", scan);
+
+            boolean isValid = height >= 1 && height <= 8;
+
+            if (isValid)
+                break;
+
+            System.out.println("Height must be between range 1 and 8");
+        }
+
+        for (int i = 0; i < height; i++) {
+            for (int j = height -1; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("#");
+            }
+
+            System.out.print("\n");
+        }
+    }
+}
