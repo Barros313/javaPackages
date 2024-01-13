77c898ed7d2c8a9c674948079e324198b36de99a
Barros313 <gbarros1337@gmail.com>
2024-01-12 23:20:32 -0300

    Main Class

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import exercises.*;
+import utilities.*;
+
 public class Main {
     public static void main(String args[]) {
-        Scrabble.main(args);
+        Clear.clearScreen();
+
+        MarioLess.marioLess(args);
+        MarioMore.marioMore(args);
+        Scrabble.scrabble(args);
     }
 }
