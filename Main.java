20f8e9e4b4a407d6920131d481c68d6568e077b3
Barros313 <gbarros1337@gmail.com>
2024-01-12 23:54:38 -0300

    Using main function to call cs50 exercises as classes.

--- Main.java
+++ Main.java
@@ -5,8 +5,38 @@ public class Main {
     public static void main(String args[]) {
         Clear.clearScreen();
 
-        MarioLess.marioLess(args);
-        MarioMore.marioMore(args);
-        Scrabble.scrabble(args);
+        if (args.length < 1) {
+            System.out.println("Usage: java 'classname'");
+            return;
+        }
+
+        String classname = args[0];
+
+        String parameters[] = new String[args.length - 1];
+        for (int i = 1; i < args.length; i++) {
+            parameters[i - 1] = args[i];
+        }
+
+        switch (classname) {
+            case "Cash":
+                Cash.cashFunction(args);
+                break;
+            case "Credit":
+                Credit.credit(args);
+                break;
+            case "MarioLess":
+                MarioLess.marioLess(args);
+                break;
+            case "MarioMore":
+                MarioMore.marioMore(args);
+                break;
+            case "Scrabble":
+                Scrabble.scrabble(args);;
+                break;
+            default:
+                System.out.println("Class not found.");
+                return;
+        }
+        System.out.println("Finished.");
     }
 }
