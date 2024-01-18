5f0fb22cd22669038ceda5173ce9478aab9d5b9c
Gabriel Barros <gbarros1337@gmail.com>
2024-01-18 10:37:41 -0300

    Added alphabet contants

---
+++ utilities/constants/Constant.java
@@ -0,0 +1,16 @@
+package utilities.constants;
+
+public class Constant {
+    public static class Length {
+        public int Alphabet(String language) {
+            switch (language) {
+                case "english":
+                    return 26;
+                case "portuguese":
+                    return 26;
+                default:
+                    return 26;
+            }
+        }
+    }
+}
