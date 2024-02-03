16fc9f7d5e4f002581f6f51160574c43f1e99463
Gabriel Barros <gbarros1337@gmail.com>
2024-02-03 10:16:13 -0300

    Added Substitution class to Main class

--- Main.java
+++ Main.java
@@ -5,8 +5,28 @@ public class Main {
     public static void main(String args[]) {
         Clear.clearScreen();
 
+        final int numberOfClasses = 9;
+
+        final String classes[] = new String[numberOfClasses];
+
+        classes[0] = "Cash";
+        classes[1] = "Credit";
+        classes[2] = "MarioLess";
+        classes[3] = "MarioMore";
+        classes[4] = "Scrabble";
+        classes[5] = "Readability";
+        classes[6] = "Caesar";
+        classes[7] = "Bulbs";
+        classes[8] = "Substitution";
+
         if (args.length < 1) {
-            System.out.println("Usage: java 'classname'");
+            System.out.print("Class availables: \n\n");
+
+            for (String i: classes) {
+                System.out.println(i);
+            }
+
+            System.out.println("\nUsage: java 'class'");
             return;
         }
 
@@ -41,6 +61,9 @@ public class Main {
                 break;
             case "Bulbs":
                 Bulbs.bulbs(parameters);
+                break;
+            case "Substitution":
+                Substitution.substituion(parameters);
                 break;
             default:
                 System.out.println("Class not found.");
